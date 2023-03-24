03532a977552894bf3d99e03365fa50b423bdab7
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-03-24 22:26:03 +0530

    Create EvenOdd.cpp

---
+++ c++/EvenOdd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int input;
+    int output;
+    cout<<"Enter the Input value to check if it is EVEN or ODD\n";
+    cin>>input;
+    if(input%2==0)
+        cout<<"EVEN";
+    else
+        cout<<"ODD";
+}
